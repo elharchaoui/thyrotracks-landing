fc50d57db196f6f0729ec135cf2fdf715a52d7b2
elharchaoui <med.el.harchaoui@gmail.com>
2025-06-19 00:16:38 +0200

    feat: add @vercel/analytics dependency and integrate Analytics component in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
+import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,6 +25,7 @@ export default function RootLayout({
         <Header /> 
         <main>{children}</main>
         {/* Footer is now part of the page's children */}
+        <Analytics />
       </body>
     </html>
   );
